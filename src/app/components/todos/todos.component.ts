0f81f5d80985bcd11794fba0967dc7af6d201f4b
Brian Thomas <bthomas@imaginuity.com>
2021-04-13 09:02:43 -0500

    added simple routing

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -24,4 +24,10 @@ export class TodosComponent implements OnInit {
     // delete from server
     this.todoService.deleteTodo(todo).subscribe();
   }
+
+  addTodo(todo: Todo) {
+    this.todoService.addTodo(todo).subscribe((todo) => {
+      this.todos.push(todo);
+    });
+  }
 }
